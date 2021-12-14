9369e364c2a826d9a4918296cb0c243f9489beec
Mbue <93818178+MbituM@users.noreply.github.com>
2021-12-14 23:08:36 +0300

    Add files via upload

---
+++ login/login/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class frmlogin
+    Private Sub btnlogin_Click(sender As Object, e As EventArgs) Handles btnlogin.Click
+        If txtusername.Text = "admin" And txtpassword.Text = "admin" Then
+            MsgBox("Login Successful!", MsgBoxStyle.OkOnly, "Log in Form")
+            Form1.Show()
+            Me.Hide()
+        Else
+            MsgBox("Sorry Incorrect Username and Password", MsgBoxStyle.OkOnly, "Invalid")
+        End If
+    End Sub
+End Class
